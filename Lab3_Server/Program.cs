9699714b3e2ab3b5bebd75310d6a4956c5df877e
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-01 14:09:16 +0300

    [lab3] Написал лабу 3

---
+++ Lab3_Server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 8005;
+const string address = "127.0.0.1";
+
+var ipEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var clientCounter = 0;
+
+try
+{
+	listenSocket.Bind(ipEndPoint);
+	listenSocket.Listen(10);
+
+	Console.WriteLine("Сервер запущен");
+
+	while (true)
+	{
+		var handler = listenSocket.Accept();
+		Console.WriteLine($"Клиент №{clientCounter} подключился");
+
+		var thread = new Thread(() => ServeClient(handler, clientCounter++));
+		thread.Start();
+	}
+}
+catch (Exception e)
+{
+	Console.WriteLine(e);
+}
+
+void ServeClient(Socket handler, int clientId)
+{
+	while (true)
+	{
+		var builder = new StringBuilder();
+
+		var totalBytes = 0;
+		var data = new byte[255];
+		var clientHasDisconnected = false;
+
+		do
+		{
+			var bytes = handler.Receive(data);
+
+			if (bytes == 0)
+			{
+				clientHasDisconnected = true;
+				break;
+			}
+
+			builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+			totalBytes += bytes;
+		} while (handler.Available > 0);
+
+		if (clientHasDisconnected)
+		{
+			Console.WriteLine($"Клиент №{clientId} отключился");
+			break;
+		}
+
+		Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}|от №{clientId}] {builder} ({totalBytes} bytes)");
+
+		handler.Send(Encoding.Unicode.GetBytes(builder.ToString()));
+	}
+}
