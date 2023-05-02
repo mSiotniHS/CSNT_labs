b718568c5402de8f68c6a2ab1953f8b4c3336e03
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-02 20:57:40 +0300

    [lab4/Common] Задал порт и адрес

---
+++ Lab4_Common/Definitions.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Lab4_Common;
+
+public static class Definitions
+{
+	public const int Port = 8005;
+	public const string Address = "127.0.0.1";
+
+	public static readonly IPEndPoint EndPoint = new(IPAddress.Parse(Address), Port);
+}
