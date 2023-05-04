fd921ddfe7c459f90bbe61de45d1222fa7147b74
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-04 20:06:36 +0300

    co: Добавил Utilities.SplitRawPackages
    Делит получаемые подряд json-ы по частям

---
+++ Lab4_Common/Utilities.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab4_Common;
+
+public static class Utilities
+{
+	public static IEnumerable<string> SplitRawPackages(string json)
+	{
+		var parts = json.Split("}{");
+
+		for (var i = 0; i < parts.Length; i++)
+		{
+			var part = parts[i];
+
+			var isFirst = i == 0;
+			var isLast = i == parts.Length - 1;
+
+			yield return (isFirst, isLast) switch
+			{
+				(true, true) => part,
+				(true, false) => part + "}",
+				(false, true) => "{" + part,
+				(false, false) => "{" + part + "}"
+			};
+		}
+	}
+}
