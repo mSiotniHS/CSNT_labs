f9a936b9cbff8bb5d106b64c89dfe91be39fe617
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-04 20:08:56 +0300

    cl: Расписал логику работы формы

---
+++ Lab4_Client/MainForm.cs
@@ -0,0 +1,114 @@
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using Lab4_Common;
+using Message = Lab4_Common.Message;
+
+namespace Lab4_Client;
+
+public partial class MainForm : Form
+{
+	private int? _userId;
+
+	private Chat _chat;
+	private readonly Socket _socket;
+
+	private readonly Font _regular = new("Iosevka", 12, FontStyle.Regular);
+	private readonly Font _bold = new("Iosevka", 12, FontStyle.Bold);
+
+	public MainForm()
+	{
+		InitializeComponent();
+
+		_chat = new Chat(new List<Message>());
+		_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		_socket.Connect(Definitions.EndPoint);
+
+		_userId = null;
+
+		var receivingThread = new Thread(ReceiveUpdates);
+		receivingThread.Start();
+	}
+
+	private void SendButton_Click(object sender, EventArgs e)
+	{
+		var text = MessageTextBox.Text.Trim();
+		if (text == string.Empty) return;
+
+		_socket.Send(
+			Encoding.Unicode.GetBytes(
+				PackageWrapper.Wrap(text).Serialize()));
+		MessageTextBox.Clear();
+	}
+
+	private void ReceiveUpdates()
+	{
+		while (true)
+		{
+			var rawPackages = Utilities.SplitRawPackages(GetData());
+
+			foreach (var rawPackage in rawPackages)
+			{
+				var package = PackageWrapper.Parse(rawPackage);
+				package.Switch(
+					userId => _userId = userId,
+					chat =>
+					{
+						_chat = chat;
+						UpdateChatBox(_chat.Messages);
+					},
+					message =>
+					{
+						_chat.Messages.Add(message);
+						UpdateChatBox(new []{ message });
+					},
+					_ => throw new UnreachableException("Клиенту пришло сообщение от другого клиента")
+				);
+			}
+		}
+	}
+
+	private string GetData()
+	{
+		var answer = new byte[256];
+		var builder = new StringBuilder();
+
+		do
+		{
+			var bytes = _socket.Receive(answer, answer.Length, 0);
+			builder.Append(Encoding.Unicode.GetString(answer, 0, bytes));
+		} while (_socket.Available > 0);
+
+		return builder.ToString();
+	}
+
+	private void UpdateChatBox(IEnumerable<Message> messages)
+	{
+		var sorted = messages
+			.OrderBy(message => message.DateTime);
+
+		foreach (var message in sorted)
+		{
+			ChatBox.SelectionAlignment =
+				message.UserId == _userId
+					? HorizontalAlignment.Right
+					: HorizontalAlignment.Left;
+
+			ChatBox.SelectionFont = _bold;
+			ChatBox.SelectedText = $"User {message.UserId} [{message.DateTime.ToShortTimeString()}]\n";
+
+			ChatBox.SelectionFont = _regular;
+			ChatBox.SelectedText = message.Content;
+
+			ChatBox.SelectedText = "\n\n";
+		}
+
+		ChatBox.ScrollToCaret();
+	}
+
+	private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+	    _socket.Shutdown(SocketShutdown.Both);
+	    _socket.Close();
+    }
+}
